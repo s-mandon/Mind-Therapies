6928a627e372c5ee3c17dbb07f7a30b55d495cfa
s-mandon <sarahmandon@hotmail.co.uk>
2019-07-02 18:53:34 +0100

    NM

---
+++ js/arrow.js
@@ -0,0 +1,63 @@
+(function(document){
+    var div = document.getElementById('btn');
+    var icon = document.getElementById('icon');
+    var open = false;
+  
+    div.addEventListener('click', function(){
+      if(open){
+        icon.className = 'fa fa-chevron-right';  
+      } else{
+        icon.className = 'fa fa-chevron-right open';
+      }
+  
+      open = !open;
+    });
+  })(document);
+
+  (function(document){
+    var div = document.getElementById('btn2');
+    var icon = document.getElementById('icon2');
+    var open = false;
+  
+    div.addEventListener('click', function(){
+      if(open){
+        icon.className = 'fa fa-chevron-right';  
+      } else{
+        icon.className = 'fa fa-chevron-right open';
+      }
+  
+      open = !open;
+    });
+  })(document);
+
+  (function(document){
+    var div = document.getElementById('btn3');
+    var icon = document.getElementById('icon3');
+    var open = false;
+  
+    div.addEventListener('click', function(){
+      if(open){
+        icon.className = 'fa fa-chevron-right';  
+      } else{
+        icon.className = 'fa fa-chevron-right open';
+      }
+  
+      open = !open;
+    });
+  })(document);
+
+  (function(document){
+    var div = document.getElementById('btn4');
+    var icon = document.getElementById('icon4');
+    var open = false;
+  
+    div.addEventListener('click', function(){
+      if(open){
+        icon.className = 'fa fa-chevron-right';  
+      } else{
+        icon.className = 'fa fa-chevron-right open';
+      }
+  
+      open = !open;
+    });
+  })(document);

5720c61d7b81f6773f625d8df4c48e43e44a0d04
s-mandon <sarahmandon@hotmail.co.uk>
2019-07-03 11:29:39 +0100

    NM

--- js/arrow.js
+++ js/arrow.js
@@ -1,63 +1,35 @@
-(function(document){
-    var div = document.getElementById('btn');
-    var icon = document.getElementById('icon');
-    var open = false;
-  
-    div.addEventListener('click', function(){
-      if(open){
-        icon.className = 'fa fa-chevron-right';  
-      } else{
-        icon.className = 'fa fa-chevron-right open';
-      }
-  
-      open = !open;
-    });
-  })(document);
+document.getElementById('wrapper').onclick = function() {
 
-  (function(document){
-    var div = document.getElementById('btn2');
-    var icon = document.getElementById('icon2');
-    var open = false;
-  
-    div.addEventListener('click', function(){
-      if(open){
-        icon.className = 'fa fa-chevron-right';  
-      } else{
-        icon.className = 'fa fa-chevron-right open';
-      }
-  
-      open = !open;
-    });
-  })(document);
+    var className = ' ' + wrapper.className + ' ';
 
-  (function(document){
-    var div = document.getElementById('btn3');
-    var icon = document.getElementById('icon3');
-    var open = false;
-  
-    div.addEventListener('click', function(){
-      if(open){
-        icon.className = 'fa fa-chevron-right';  
-      } else{
-        icon.className = 'fa fa-chevron-right open';
-      }
-  
-      open = !open;
-    });
-  })(document);
+    this.className = ~className.indexOf(' active ') ?
+                         className.replace(' active ', ' ') :
+                         this.className + ' active';
+};
 
-  (function(document){
-    var div = document.getElementById('btn4');
-    var icon = document.getElementById('icon4');
-    var open = false;
-  
-    div.addEventListener('click', function(){
-      if(open){
-        icon.className = 'fa fa-chevron-right';  
-      } else{
-        icon.className = 'fa fa-chevron-right open';
-      }
-  
-      open = !open;
-    });
-  })(document);
+document.getElementById('wrapper2').onclick = function() {
+
+    var className = ' ' + wrapper2.className + ' ';
+
+    this.className = ~className.indexOf(' active ') ?
+                         className.replace(' active ', ' ') :
+                         this.className + ' active';
+}
+
+document.getElementById('wrapper3').onclick = function() {
+
+    var className = ' ' + wrapper3.className + ' ';
+
+    this.className = ~className.indexOf(' active ') ?
+                         className.replace(' active ', ' ') :
+                         this.className + ' active';
+}
+
+document.getElementById('wrapper4').onclick = function() {
+
+    var className = ' ' + wrapper4.className + ' ';
+
+    this.className = ~className.indexOf(' active ') ?
+                         className.replace(' active ', ' ') :
+                         this.className + ' active';
+}
